3df709cb03ecd5a2d6c501715c819aebf0f9b5da
Daniel D <ddunnemann@gmail.com>
2024-06-22 19:59:21 -0500

    I've added a rough skeleton of the basic game logic. Needs some further refinement but this should give a decent starting point for the game logic.

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,18 @@
 const addPlayerBtn = document.querySelector("#add-player-btn");
-const startBtn = document.getElementById("start-game-btn");
 const landingPage = document.getElementById("landing")
-const gamePage = document.getElementById("game-page")
-const resultsPage = document.getElementById("results-page")
-const guessBtn = document.getElementById("guess")
-const playAgain = document.getElementById("play-again")
+const startBtn = document.getElementById("start-game-btn");
+
+const gamePage = document.getElementById("game-page");
+const guessBtn = document.getElementById("guess");
+
+const resultsPage = document.getElementById("results-page");
+const roundDisplay = document.getElementById("round-display");
+
+const playAgain = document.getElementById("play-again");
+
+// Initialize game state as Round 1.
+let roundCount = 1;
+
 let players = JSON.parse(localStorage.getItem("playersData"));
 if (players === null) {
   players = [];
@@ -43,29 +51,47 @@ const logStoredPlayers = function () {
   }
 };
 
+// This function begins when Start Game button on Landing Page is pressed.
 const gameStart = function() {
-  landingPage.setAttribute("class","page out");
-  setInterval(gamePage.setAttribute("class", "page load"), 500);
-  setInterval(gamePage.setAttribute("class", "page in"),500);
-};
+    roundDisplay.innerHTML = `Round:${roundCount}/10`;
 
+    // This is the screen transition script.
+    // It applies CSS classes that effectively slide the current containers off screen to be replaced by the next screen.
+    landingPage.setAttribute("class","page out");
+    setInterval(gamePage.setAttribute("class", "page load"), 500);
+    setInterval(gamePage.setAttribute("class", "page in"),500);
+    };
+
+// TODO: I started to put together a function to call when checking score player enters versus their guessed score. It is incomplete. This would probably be a good place to have API fetch request to pull Rotten Tomatoes scores from OMDB.
+const guessCheck = function() {
+    if (guessedScore === actualScore) {}
+    };
+
+// This function is called when the Submit button on the Gameplay screen is pressed. It will increment the current 'Round' up to 10. After Round 10 the final round it will transition to the Game Results screen, which would be the final screen. WIP.
+
+// TODO: I suggest this function also being the place to add our API fetch requests to populate the Movie Title, Poster, and Plot Summary from TMDB.
 const gameResults = function() {
-  gamePage.setAttribute("class","page out");
-  setInterval(resultsPage.setAttribute("class", "page load"), 500);
-  resultsPage.setAttribute("class", "page in");
-};
+    if (roundCount !== 10) {
+    roundCount++;
+    roundDisplay.innerHTML = `Round:${roundCount}/10`;
+    console.log(roundCount);
+    } else if (roundCount === 10) {
+    gamePage.setAttribute("class","page out");
+    setInterval(resultsPage.setAttribute("class", "page load"), 500);
+    resultsPage.setAttribute("class", "page in");
+    }
+    };
 
 const restartGame = function() {
-  resultsPage.setAttribute("class","page out");
-  setInterval(landingPage.setAttribute("class", "page load"), 500);
-  landingPage.setAttribute("class", "page in");
-};
+    roundCount = 1;
+    resultsPage.setAttribute("class","page out");
+    setInterval(landingPage.setAttribute("class", "page load"), 500);
+    landingPage.setAttribute("class", "page in");
+    };
 
 
+// Event listeners below. The names should be helpful in discerning which is which.
 addPlayerBtn.addEventListener("click", trackPlayersData);
-
 startBtn.addEventListener("click", gameStart);
-
 guessBtn.addEventListener("click", gameResults);
-
 playAgain.addEventListener("click", restartGame);
