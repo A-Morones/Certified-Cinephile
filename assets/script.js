ff8c965cc3dd5014a3eac72893f045919584d7a9
MrTdevo <168947566+MrTdevo@users.noreply.github.com>
2024-06-30 19:57:24 -0400

    AMking sure players read the rules

--- assets/script.js
+++ assets/script.js
@@ -226,9 +226,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const span = document.getElementsByClassName("close")[0];
 
   // When the user clicks the button, open the modal
-  btn.onclick = function () {
-    modal.style.display = "block";
-  };
+  // btn.onclick = function () {
+  //   modal.style.display = "block";
+  // };
 
   // When the user clicks on <span> (x), close the modal
   span.onclick = function () {
@@ -240,4 +240,51 @@ document.addEventListener("DOMContentLoaded", function () {
   //     startBtn.removeAttribute(disabled);
   //   }
   // }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add("is-active");
+  }
+
+  function closeModal($el) {
+    $el.classList.remove("is-active");
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll(".modal") || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll(".js-modal-trigger") || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+
+    $trigger.addEventListener("click", () => {
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  (
+    document.querySelectorAll(
+      ".modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button"
+    ) || []
+  ).forEach(($close) => {
+    const $target = $close.closest(".modal");
+
+    $close.addEventListener("click", () => {
+      closeModal($target);
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      closeAllModals();
+    }
+  });
 });
