ec73f36fdbdb8cfac01a27d21ed9fa24f3cd83db
MrTdevo <168947566+MrTdevo@users.noreply.github.com>
2024-06-30 18:28:34 -0400

    Makking sure user read the rule before proceeding

--- assets/script.js
+++ assets/script.js
@@ -233,12 +233,11 @@ document.addEventListener("DOMContentLoaded", function () {
   // When the user clicks on <span> (x), close the modal
   span.onclick = function () {
     modal.style.display = "none";
+    addPlayerBtn.disabled = false;
   };
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
+  // function enableStartGame() {
+  //   if (btn && addPlayerBtn) {
+  //     startBtn.removeAttribute(disabled);
+  //   }
+  // }
 });
